9512ad3083d15aaf64a0441ff944046b9ecb5269
Shending-Help <yusufcheetos99@gmail.com>
2022-08-29 18:06:22 +0200

    words endpoint

---
+++ src/handlers/questions.ts
@@ -0,0 +1,20 @@
+import { Question, Questions } from "../models/question";
+import express, { Request, Response } from "express";
+
+const questionStore = new Questions();
+
+const questionsRoutes = (app: express.Application) => {
+  app.get("/questions", showTenQuestions);
+};
+
+const showTenQuestions = async (_req: Request, res: Response) => {
+  try {
+    const questions = await questionStore.showTenRandom();
+    res.json(questions);
+  } catch (err) {
+    res.status(500);
+    res.json(err);
+  }
+};
+
+export default questionsRoutes;
