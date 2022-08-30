06e18161de617fc64f3c022ed815724ed38325c2
Shending-Help <yusufcheetos99@gmail.com>
2022-08-30 19:34:47 +0200

    quiz feedback after answer

--- quiz-frontend/src/components/Quiz.js
+++ quiz-frontend/src/components/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef, useEffect } from "react";
 import { Questions } from "../helpers/TempData";
 import { QuizContext } from "../helpers/Contexts";
 const Quiz = () => {
@@ -10,9 +10,11 @@ const Quiz = () => {
     if (Questions[currQuestion].pos === answer) {
       setScore(score + 1);
     }
-    setCurrQuestion(currQuestion + 1);
-  };
 
+    setCurrQuestion(currQuestion + 1);
+    setAnswer("");
+  };
+  const buttonRef = useRef(null);
   const finishQuiz = () => {
     if (Questions[currQuestion].pos === answer) {
       setScore(score + 1);
@@ -20,34 +22,60 @@ const Quiz = () => {
     setQuizState("rank");
   };
 
+  const feedBack = () => {
+    if (Questions[currQuestion].pos === answer) {
+      return <h2>Correct!</h2>;
+    }
+    if (answer === "" || answer === undefined || answer === null) {
+      return <h2>Please select an answer</h2>;
+    } else {
+      console.log(answer);
+      return <h2>Incorrect!</h2>;
+    }
+  };
+
   return (
     <div className="Quiz">
+      <label for="file">Quiz Progress:</label>
+      <progress id="file" value={(currQuestion / 10) * 100} max="100">
+        {" "}
+        {(currQuestion / 10) * 100}{" "}
+      </progress>
       <h1> {Questions[currQuestion].word} </h1>
       <div className="options">
         <button
+          ref={buttonRef}
           onClick={() => {
             setAnswer("verb");
+            feedBack();
           }}
         >
           verb
         </button>
         <button
+          ref={buttonRef}
           onClick={() => {
             setAnswer("noun");
+            feedBack();
           }}
         >
           noun
         </button>
         <button
+          onChange={() => {}}
+          ref={buttonRef}
           onClick={() => {
             setAnswer("adverb");
+            feedBack();
           }}
         >
           adverb
         </button>
         <button
+          ref={buttonRef}
           onClick={() => {
             setAnswer("adjective");
+            feedBack();
           }}
         >
           adjective
@@ -65,6 +93,8 @@ const Quiz = () => {
       ) : (
         <button onClick={nextQuestion}> Next Question </button>
       )}
+
+      {feedBack()}
     </div>
   );
 };
--- quiz-frontend/src/components/RankScreen.js
+++ quiz-frontend/src/components/RankScreen.js
@@ -1,5 +1,20 @@
+import React, { useState, useContext } from "react";
+import { Questions } from "../helpers/TempData";
+import { QuizContext } from "../helpers/Contexts";
+
 const Rank = () => {
-  return <div className="rank"> Rank </div>;
+  const { score, setScore, setQuizState } = useContext(QuizContext);
+  const restartQuiz = () => {
+    setScore(0);
+    setQuizState("menu");
+  };
+  return (
+    <div className="Rank">
+      <h1> You scored {score} out of 10! </h1>
+
+      <button onClick={restartQuiz}>Restart Quiz</button>
+    </div>
+  );
 };
 
 export default Rank;
