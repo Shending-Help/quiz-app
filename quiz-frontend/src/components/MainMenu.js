b4b0b5468089322d2b52563db3e23aa14885cb24
Shending-Help <yusufcheetos99@gmail.com>
2022-08-31 13:27:32 +0200

    comments and refactoring

--- quiz-frontend/src/components/MainMenu.js
+++ quiz-frontend/src/components/MainMenu.js
@@ -1,5 +1,7 @@
 import { QuizContext } from "../helpers/Contexts";
 import React, { useContext } from "react";
+
+//this component is used to display the main menu it has a button when clicked it changes the state of the quiz to start
 const MainMenu = () => {
   const { setQuizState } = useContext(QuizContext);
   return (
